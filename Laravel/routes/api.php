a601a31b5cf190700bf95b6392c7ae453981dd2f
DAIKO\fukudomeh <fukudomeh@daikoiws.co.jp>
2021-12-02 12:46:13 +0900

    no message

--- Laravel/routes/api.php
+++ Laravel/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::middleware(['cors'])->group(function () {
+    Route::options('accounts', function () {
+        return response()->json();
+    });
+
+    Route::post('accounts', 'AccountController@create');
 });
