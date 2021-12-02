1a65a9d607f884aaefc9df94809a88071a5f9942
DAIKO\fukudomeh <fukudomeh@daikoiws.co.jp>
2021-12-02 11:30:59 +0900

    CORS回避用

---
+++ Laravel/app/Http/Middleware/Cors.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        // すべてのレスポンスに CORS 用のヘッダーを追加する必要はないので URL から判断する
+        $paths = explode('/', \Request::getPathInfo());
+        if ($paths[1] === 'api') {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST,PUT,DELETE')
+                // ->header('Access-Control-Allow-Headers', 'Accept, X-Requested-With, Origin, Content-Type');
+                ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+        }
+        return $next($request);
+    }
+}
